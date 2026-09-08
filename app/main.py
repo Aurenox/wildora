9aedee33b22f6f3f241ccdf5f911828de2771572
Saurav B <saurav04atg@gmail.com>
2026-09-09 03:07:50 +0530

    Fixed

--- app/main.py
+++ app/main.py
@@ -47,8 +47,13 @@ STATIC_DIR = next(
 )
 
 
-UPLOAD_DIR = STATIC_DIR / "uploads"
-CREATURE_DIR = STATIC_DIR / "creatures"
+UPLOAD_DIR = Path("/tmp/wildora/uploads")
+CREATURE_DIR = Path("/tmp/wildora/creatures")
+
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+CREATURE_DIR.mkdir(parents=True, exist_ok=True)
+# UPLOAD_DIR = STATIC_DIR / "uploads"
+# CREATURE_DIR = STATIC_DIR / "creatures"
 
 
 # UPLOAD_DIR.mkdir(
